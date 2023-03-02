fb3b627eb1b0de19792b956189902d514e697e64
yonnie-park <juon0805@gmail.com>
2023-03-02 23:37:28 +0900

    4일차

--- 괄호회전하기.js
+++ 괄호회전하기.js
@@ -1,3 +1,33 @@
+function isValid(arr) {
+    const brackets = {
+        "}": "{",
+        ")": "(",
+        "]": "["
+    }
+    let stack = []
+    for (let i = 0; i < arr.length; i++) {
+        if (brackets[arr[i]] === undefined) stack.push(arr[i])
+        else {
+            if (stack[stack.length - 1] != brackets[arr[i]]) return false
+            stack.pop()
+        }
+    }
+    if (stack.length) return false
+    return true
+}
+
 function solution(s) {
 
+    let brackets_arr = s.split("")
+    let ans = 0
+
+    for (let i = 0; i < s.length; i++) {
+        if (isValid(brackets_arr)) ans++
+            brackets_arr.push(brackets_arr.shift())
+    }
+    return ans
 }
+console.log(solution("[](){}"))
+console.log(solution("}]()[{"))
+console.log(solution("[)(]"))
+console.log(solution("}}}"))
